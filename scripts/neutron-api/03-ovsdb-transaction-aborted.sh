9486fd1f165ea50794fc441737e1efa090be807f
Edward Hope-Morley <edward.hope-morley@canonical.com>
2025-04-16 15:05:32 +0100

    Fix 03-ovsdb-transaction-aborted.sh search

--- scripts/neutron-api/03-ovsdb-transaction-aborted.sh
+++ scripts/neutron-api/03-ovsdb-transaction-aborted.sh
@@ -9,7 +9,7 @@
 
 SCRIPT_HEADER neutron.plugins.ml2.drivers.ovn.mech_driver.ovsdb.impl_idl_ovn
 
-row_expr="s/$EXPR_LOG_DATE_GROUP_TIME $EXPR_LOG_CONTEXT Transaction aborted/\1/p"
-process_log_simple $LOG $DATA_TMP $CSV_PATH "$row_expr" true txn_aborted
+row_expr="s/$EXPR_LOG_DATE_GROUP_TIME $EXPR_LOG_CONTEXT Transaction aborted. .+/\1/p"
+process_log_tally $LOG $DATA_TMP $CSV_PATH "$row_expr" true txn_aborted
 
 SCRIPT_FOOTER txn_aborted
