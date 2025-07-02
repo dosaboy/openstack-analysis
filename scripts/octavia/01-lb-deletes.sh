f344612379eb25facb2d5db191f89f466982374b
Edward Hope-Morley <edward.hope-morley@canonical.com>
2025-07-02 11:32:43 +0100

    Add octavia lb deletes

---
+++ scripts/octavia/01-lb-deletes.sh
@@ -0,0 +1,13 @@
+# Description: get number of loadbalancer delete requests.
+
+# NOTE: only run this for octavia-worker logs
+LOG_NAME_FILTER=octavia-worker.log
+LOG_MODULE=octavia.controller.queue.v1.endpoints
+Y_LABEL=lb-deletes
+PLOT_TYPE=bar_stacked
+
+main ()
+{
+    row_expr="^$EXPR_LOG_DATE_GROUP_DATE_AND_TIME [0-9]+ \w+ $LOG_MODULE \[-\] Deleting load balancer '\S+'..."
+    process_log_tally $LOG $DATA_TMP $CSV_PATH "$row_expr" true $Y_LABEL
+}
