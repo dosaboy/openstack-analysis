10d1ecbcc0b8d6a9eb476ae0a9a59777f2720a4c
Edward Hope-Morley <edward.hope-morley@canonical.com>
2025-07-09 15:16:15 +0100

    Fix nova network-vif-plugged-timout plot

--- scripts/nova-compute/12-network-vif-plugged-timeout.sh
+++ scripts/nova-compute/12-network-vif-plugged-timeout.sh
@@ -3,12 +3,13 @@
 
 # NOTE: only run this for nova-compute logs
 LOG_NAME_FILTER=nova-compute.log
-LOG_MODULE=nova.virt.libvirt.driver
+LOG_MODULE=nova.compute.manager
 Y_LABEL=network-vif-plugged-timeouts
 PLOT_TYPE=bar_stacked
 
 main ()
 {
-    row_expr="^$EXPR_LOG_DATE_GROUP_DATE_AND_TIME $EXPR_LOG_CONTEXT $EXPR_LOG_INSTANCE_UUID Timeout waiting for \[\('network-vif-plugged'\, .+"
-    process_log_tally $LOG $DATA_TMP $CSV_PATH "$row_expr" true $Y_LABEL
+    col_expr="^$EXPR_LOG_DATE $EXPR_LOG_CONTEXT $EXPR_LOG_INSTANCE_UUID_GROUP_UUID Timeout waiting for \['network-vif-plugged-.+'\].+"
+    row_expr="^$EXPR_LOG_DATE_GROUP_DATE_AND_TIME $EXPR_LOG_CONTEXT $EXPR_LOG_INSTANCE_UUID_INSERT_UUID Timeout waiting for \['network-vif-plugged-.+'\].+"
+    process_log_tally_multicol $LOG $DATA_TMP $CSV_PATH "$col_expr" "$row_expr" 0 true
 }
